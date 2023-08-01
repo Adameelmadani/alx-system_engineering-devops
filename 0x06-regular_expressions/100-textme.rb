330ac1017e5778ab00d84d6aa9a3822d159ffe34
adam <adam@.gmail.com>
2023-08-01 16:41:20 -0700

    100.task

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/(?<=from\:)(\w+|\+\d{1,})|(?<=to\:)(\w+|\+\d{1,})|((?<=flags\:).*?(?=\]))/).join(",")
+puts ARGV[0].scan(/(?<=from\:).*?(?=\])|(?<=to\:).*?(?=\])|(?<=flags\:).*?(?=\])/).join(",")
