562f1bccadc34f01fcb33b225261c8a43ef16bc2
Adam <adamelmadani0129@gmail.com>
2023-09-25 00:01:58 +0000

    100.task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,6 +1,5 @@
 #Configuring config file
 file_line { 'Add new line':
-  ensure => present,
   path   => 'etc/ssh/ssh_config',
   line   => "Host *\n  IdentityFile ~/.ssh/school\n  PasswordAuthentication no",
 }
