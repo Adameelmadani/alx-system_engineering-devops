7da2f9545191617fe9aa58c8981bc2bec2e09634
Adam <adamelmadani0129@gmail.com>
2023-09-25 00:14:19 +0000

    100.task

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,8 @@
-# Configuring config file
-file_line { 'Add new line':
-  path => 'etc/ssh/ssh_config',
+# Client-side configuration
+file_line{'Configuration':
+  path => '/etc/ssh/ssh_config',
   line => 'Host 100.25.164.203
+    HostName 100.25.164.203
     User ubuntu
     IdentityFile ~/.ssh/school
     PasswordAuthentication no',
