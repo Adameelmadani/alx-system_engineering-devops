74655ef06f7371c201926ee68749626b1d29528a
Adam <adamelmadani0129@gmail.com>
2023-10-24 00:15:02 +0000

    0.task fix

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,22 +7,18 @@ import sys
 
 
 if __name__ == "__main__":
-    user_id = int(sys.argv[1])
+    user_id = sys.argv[1]
     user_url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
-    user_r = requests.get(user_url)
-    user_json = user_r.json()
+    user_name = requests.get(user_url).json().get('name')
 
     todo_url = "https://jsonplaceholder.typicode.com/todos"
-    todo_r = requests.get(todo_url)
-    todo_json = todo_r.json()
-
-    user_name = user_json.get('name')
+    todo_json = requests.get(todo_url).json()
 
     tasks = []
     n_d = 0
     n = 0
     for do in todo_json:
-        if do.get('userId') == user_id:
+        if do.get('userId') == int(user_id):
             if do.get('completed') is True:
                 tasks.append(do.get('title'))
                 n_d += 1
