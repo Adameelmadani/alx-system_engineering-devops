d46010f86cddf73974eb59cdaa1826a983aeda71
Adam <adamelmadani0129@gmail.com>
2023-11-12 10:19:36 +0000

    2.task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,16 +9,17 @@ def recurse(subreddit, hot_list=[], after=""):
     """
     This is our function
     """
+    if subreddit is None:
+        return None
     url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
     user_agent = {"User-Agent": "ALX project about advanced api"}
     r = requests.get(url, params={"after": after}, headers=user_agent)
-    if r.status_code == 404:
-        return None
-    r_json = r.json()
-    after = r_json.get("data").get("after")
-    if not after:
-        return hot_list
-    ch_list = r_json.get("data").get("children")
-    for ch_l in ch_list:
-        hot_list.append(ch_l.get("data").get("title"))
-    return recurse(subreddit, hot_list, after)
+    if r.status_code == 200:
+        r_json = r.json()
+        after = r_json.get("data").get("after")
+        if not after:
+            return hot_list
+        ch_list = r_json.get("data").get("children")
+        for ch_l in ch_list:
+            hot_list.append(ch_l.get("data").get("title"))
+        return recurse(subreddit, hot_list, after)
