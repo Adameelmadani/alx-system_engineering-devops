d5e7953c7fb6ce174b2413af83149280d5860586
Adam <adamelmadani0129@gmail.com>
2023-11-12 09:45:08 +0000

    2-recurse.py

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -9,14 +9,16 @@ def recurse(subreddit, hot_list=[], after=""):
     """
     This is our function
     """
-    url = "https://www.reddit.com/r/{}/json?after={}".format(subreddit, after)
+    url = "https://www.reddit.com/r/{}.json?after={}".format(subreddit, after)
     r = requests.get(url)
     if r.status_code == 404:
         return None
     r_json = r.json()
     ch_list = r_json.get("data").get("children")
-    for ch_l in ch_list:
-        ch_l = ch_l.get("data").get("title")
+    if after == "" and len(ch_list) == 0:
+        return None
+    for i in range(0, len(ch_list)):
+        ch_list[i] = ch_list[i].get("data").get("title")
     hot_list += ch_list
     if r_json.get("data").get("after", None):
         return recurse(subreddit, hot_list, r_json.get("data").get("after"))
